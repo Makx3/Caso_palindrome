50269816d40b8c4c41292b7c17a384371dd30ad0
Makx3 <142506889+Makx3@users.noreply.github.com>
2023-09-13 10:36:05 -0300

    Update Main.java

--- Palindromo/src/main/java/org/example/Main.java
+++ Palindromo/src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese una cadena: ");
+        String cadena = scanner.nextLine();
+
+        boolean resultado = esPalindromo(cadena);
+
+        if (resultado) {
+            System.out.println("La cadena es un palindromo.");
+        } else {
+            System.out.println("La cadena no es un palindromo o esta vacia.");
+        }
+    }
     public static boolean esPalindromo(String cadena) {
         // Elimina espacios en blanco y convierte la cadena a minúsculas
         cadena = cadena.replaceAll("\\s", "").toLowerCase();
